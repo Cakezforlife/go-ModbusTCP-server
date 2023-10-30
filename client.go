b227deed76dd764ef947a68fc992efbb7e6da6dc
Cakezforlife <gconto10@gmail.com>
2023-10-30 11:37:45 -0500

    initial client setup

--- client.go
+++ client.go
@@ -3,10 +3,10 @@ package main
 import (
 	"os"
 	"fmt"
+	"net"
 )
 
-func main() {
-	args := os.Args
+func client(args []string) {
 	if len(args) < 3{
 		fmt.Println("Expected host and port")
 		fmt.Println("Usage\n" +
@@ -16,5 +16,31 @@ func main() {
 		os.Exit(1)
 	}
 	host := fmt.Sprintf("%s:%s", args[1], args[2])
-	fmt.Println(host)
+	fmt.Println("Starting Client connection to", host)
+
+	c, err := net.Dial("TCP", host)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error connecting to host %v\n", err)
+	}
+
+	responseMBAP := types.MBAPHeader{
+		TransactionIdentifier: ADU.MBAP.TransactionIdentifier,
+		ProtocolIdentifier: ADU.MBAP.ProtocolIdentifier,
+		Length: 4,
+		UnitIdentifier: 2,
+	}
+	responsePDU := types.ModbusPDU {
+		FunctionCode: 6,
+		Data: []byte{1,1},
+	}
+	responseADU := types.ModbusADU{MBAP: responseMBAP, PDU: responsePDU}
+	fmt.Printf("Sending: %s\n", responseADU.ToString())
+
+	responseBytes, err := responseADU.ToBinary()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ToBinary Failed %v", err)
+	}
+	fmt.Printf("%v\n", helper.FormatByteSliceAsHexSliceString(responseBytes))
+
+	c.Write(responseBytes)
 }
